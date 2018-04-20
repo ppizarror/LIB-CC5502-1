30331e01e14a1ccd564e77731dd4e361eb7c07b1
Pablo Pizarro <pablo.pizarro@ing.uchile.cl>
2018-04-20 03:30:02 +0100

    Agrega test precisión segmento

--- tests/test-segmento.cpp
+++ tests/test-segmento.cpp
@@ -48,6 +48,75 @@ void testInterseccion() {
 }
 
 /**
+ * Retorna texto SI/NO de acuerdo a 1/0
+ * @return
+ */
+std::string boolString(int v) {
+    if (v) {
+        return "SI";
+    } else {
+        return "NO";
+    }
+}
+
+/**
+ * Test de precisión.
+ */
+void testPrecision() {
+    /** Crea los puntos
+     *  p3(2,0.4) debe estar en Segmento
+     *  p4(2,0.41) debe estar estrictamente a la izquierda del Segmento
+     *
+     *  1               p4         ________ p2
+     *  |       ____....p3..^^^^^^
+     *  0    p1
+     *  |
+     *  y\x--0-----1-----2-----3-----4-----5
+     */
+    Punto<int> p1_i = Punto<int>(0, 0);
+    Punto<int> p2_i = Punto<int>(5, 1);
+
+    Punto<double> p1_d = Punto<double>(0, 0);
+    Punto<double> p2_d = Punto<double>(5, 1);
+
+    // Puntos sobre el segmento
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wconversion"
+    Punto<int> pin_i = Punto<int>(2, 0.4);
+#pragma clang diagnostic pop
+    Punto<double> pin_d = Punto<double>(2, 0.4);
+
+    // Puntos a la izquierda estricto del segmento
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wconversion"
+    Punto<int> pleft_i = Punto<int>(2, 0.41);
+#pragma clang diagnostic pop
+    Punto<double> pleft_d = Punto<double>(2, 0.41);
+
+    // Puntos a la izquierda estricto del segmento MUY PEQUEÑOS
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wconversion"
+    Punto<int> pleftpeq_i = Punto<int>(2, 0.41);
+#pragma clang diagnostic pop
+    Punto<double> pleftpeq_d = Punto<double>(2, 0.4000000000000000000000000000000000000000000000000000000000000001);
+
+    // Crea los segmentos en entero y double
+    Segmento<int> s_i = Segmento<int>(p1_i, p2_i);
+    Segmento<double> s_d = Segmento<double>(p1_d, p2_d);
+
+    /**
+     * Imprime en consola.
+     */
+    std::cout << "\nTest de precision:" << std::endl;
+    std::cout << "\t[Integer] contiene (2,0.4): " << boolString(s_i.on(pin_i)) << std::endl;
+    std::cout << "\t[Double]  contiene (2,0.4): " << boolString(s_d.on(pin_d)) << std::endl;
+    std::cout << "\t(2,0.41) izquierda de [Integer]: " << boolString(s_i.left(pleft_i)) << std::endl;
+    std::cout << "\t(2,0.41) izquierda de [Double]: " << boolString(s_d.left(pleft_d)) << std::endl;
+    std::cout << "\t(2,0.400...01) izquierda de [Integer]: " << boolString(s_i.left(pleftpeq_i)) << std::endl;
+    std::cout << "\t(2,0.400...01) izquierda de [Double] : " << boolString(s_d.left(pleftpeq_d)) << std::endl;
+}
+
+/**
  * Corre los test.
  * @return
  */
@@ -57,6 +126,7 @@ int main() {
     // Carga los tests
     testLargo();
     testInterseccion();
+    testPrecision();
 
     // Retorna
     return 0;
